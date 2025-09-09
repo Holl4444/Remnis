326b97dcbc64a473228ec796bd10009f0fc794db
Holl4444 <holly.cello@gmail.com>
2025-09-09 20:39:52 +0100

    fix: deployment CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -9,7 +9,11 @@ load_dotenv()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],  # Frontend URLs
+    allow_origins=[
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+        "https://remnis.vercel.app/"
+     ],  # Frontend URLs
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],)
